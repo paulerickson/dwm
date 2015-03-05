bdbbf3acea67a501a551bb0079b99559c2caa137
Paul Erickson <paul.d.erickson@gmail.com>
2015-03-04 18:14:00 -0600

    Applying single_tagset patch to share tagset between monitors.  See http://dwm.suckless.org/patches/single_tagset

--- config.def.h
+++ config.def.h
@@ -54,6 +54,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "keepfloatingposition.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
